f84241d1ef926cb99403dd73c335abb7469530c9
Suvin T M <suvintm1515@gmail.com>
2025-10-02 16:14:35 +0530

    main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,9 @@ import { AppProvider } from "./context/AppContext";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AppProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AppProvider>
-  </React.StrictMode>
+  <AppProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AppProvider>
 );
