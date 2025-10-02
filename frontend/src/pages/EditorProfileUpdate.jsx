4b24f8407ec20d1893448ed70f165b22efe73985
Suvin T M <suvintm1515@gmail.com>
2025-10-02 23:29:13 +0530

    profileupdate page added

---
+++ frontend/src/pages/EditorProfileUpdate.jsx
@@ -0,0 +1,12 @@
+ import React from 'react'
+ 
+ const EditorProfileUpdate = () => {
+   return (
+     <div>
+       profile update page
+     </div>
+   )
+ }
+ 
+ export default EditorProfileUpdate
+ 
