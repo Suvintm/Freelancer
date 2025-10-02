a3e1a16cc17bba2e2b0444612bbdb66a5925f186
Suvin T M <suvintm1515@gmail.com>
2025-10-02 16:18:31 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage.jsx";
 import ClientHome from "./pages/clientHome.jsx";
 import EditorHome from "./pages/EditorHome.jsx";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/client-home" element={<ClientHome />} />
-        <Route path="/editor-home" element={<EditorHome />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/client-home" element={<ClientHome />} />
+      <Route path="/editor-home" element={<EditorHome />} />
+    </Routes>
   );
 }
 
