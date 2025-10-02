a67329453400993e7d78b07ac35d40a11e5a37a7
Suvin T M <suvintm1515@gmail.com>
2025-10-02 16:25:54 +0530

    main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
-// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AppProvider } from "./context/AppContext";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css"; // 👈 ensure global styles are imported
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AppProvider>
